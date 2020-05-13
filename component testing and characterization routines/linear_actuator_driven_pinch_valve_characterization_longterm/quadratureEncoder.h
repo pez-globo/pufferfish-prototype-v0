be91d4e0a3d2aacc7a3c498426fd7d1f4a09e070
Deepak Krishnamurthy <deepak90@stanford.edu>
2020-05-13 00:22:47 -0700

    Encoder class for Arduino

---
+++ component testing and characterization routines/linear_actuator_driven_pinch_valve_characterization_longterm/quadratureEncoder.h
@@ -0,0 +1,44 @@
+// Arduino class for a quadrature encoder
+
+/*
+  quadratureEncoder.h - Library for reading a quadrature encoder. 
+  Created by Deepak Krishnamurthy, May 12, 2020.
+
+  Reads the quadrature encoder signal from a pair of digital pins.
+
+  Encoder signal is read using Interrupts.
+
+  Decodes the signal to update the Encoder Tick Counts.
+  
+*/
+
+#ifndef quadratureEncoder_h
+#define quadratureEncoder_h
+
+#include "Arduino.h"
+
+class quadratureEncoder
+{
+	public:
+
+		quadratureEncoder(int pin_A, int pin_B, int mode);
+
+		void decoder();
+
+		void HandleInterrupt();
+
+		volatile long int EncoderTicks;
+		volatile int Dir;
+		
+
+	private:
+
+		volatile bool _EncoderASet;
+		volatile bool _EncoderBSet;
+		volatile bool _EncoderAPrev;
+		volatile bool _EncoderBPrev;
+
+}
+
+
+#endif
